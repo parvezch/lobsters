3d743cc9aab8096e7c7307f9a443098af7b7a4a8
parvezch <parvezchoudhury3@gmail.com>
2022-09-08 14:40:56 +0530

    update comments ui

--- lib/widgets/comment_tree.dart
+++ lib/widgets/comment_tree.dart
@@ -12,87 +12,105 @@ class CommentTree extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      controller: _controller,
-      itemCount: _comments.length,
-      itemBuilder: (context, index) {
-        return Card(
-          shape: const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
-          margin: EdgeInsets.only(
-            left: _comments[index].indentLevel > 1
-                ? (_comments[index].indentLevel - 1) * 16.0
-                : 0,
-            top: _comments[index].indentLevel > 1 ? 0 : 4.0,
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Image(
-                      height: 14.0,
-                      width: 14.0,
-                      image: NetworkImage(
-                          "https://lobste.rs${_comments[index].commentingUser.avatarUrl}"),
-                    ),
-                    const SizedBox(
-                      width: 4.0,
-                    ),
-                    Text(
-                      _comments[index].commentingUser.username,
-                      style: const TextStyle(
-                        color: Colors.blue,
-                        fontSize: 12,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 8,
-                      child: Icon(
-                        Icons.circle,
-                        size: 4,
-                        color: Colors.grey[600],
-                      ),
-                    ),
-                    Text(
-                      "${TimeElapsed.fromDateStr(_comments[index].createdAt)} ago",
-                      style: TextStyle(
-                        color: Colors.grey[600],
-                        fontSize: 12,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 8,
-                      child: Icon(
-                        Icons.circle,
-                        size: 4,
-                        color: Colors.grey[600],
-                      ),
-                    ),
-                    Text(
-                      "${_comments[index].score} upvotes",
-                      style: TextStyle(
-                        color: Colors.grey[600],
-                        fontSize: 12,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 4.0),
-                Text(
-                  _comments[index].commentPlain,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 14,
-                  ),
-                ),
-              ],
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: ListView.builder(
+        shrinkWrap: true,
+        controller: _controller,
+        itemCount: _comments.length,
+        itemBuilder: (context, index) {
+          return Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topLeft: _comments[index].indentLevel > 1
+                    ? Radius.zero
+                    : const Radius.circular(8.0),
+                topRight: _comments[index].indentLevel > 1
+                    ? Radius.zero
+                    : const Radius.circular(8.0),
+                bottomLeft: const Radius.circular(8.0),
+                bottomRight: index == _comments.length - 1
+                    ? const Radius.circular(8.0)
+                    : _comments[index + 1].indentLevel > 1
+                        ? Radius.zero
+                        : const Radius.circular(8.0),
+              ),
             ),
-          ),
-        );
-      },
+            margin: EdgeInsets.only(
+              left: _comments[index].indentLevel > 1
+                  ? (_comments[index].indentLevel - 1) * 16.0
+                  : 0,
+              top: _comments[index].indentLevel > 1 ? 0 : 4.0,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Image(
+                        height: 14.0,
+                        width: 14.0,
+                        image: NetworkImage(
+                            "https://lobste.rs${_comments[index].commentingUser.avatarUrl}"),
+                      ),
+                      const SizedBox(
+                        width: 4.0,
+                      ),
+                      Text(
+                        _comments[index].commentingUser.username,
+                        style: const TextStyle(
+                          color: Colors.blue,
+                          fontSize: 12,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 8,
+                        child: Icon(
+                          Icons.circle,
+                          size: 4,
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                      Text(
+                        "${TimeElapsed.fromDateStr(_comments[index].createdAt)} ago",
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                          fontSize: 12,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 8,
+                        child: Icon(
+                          Icons.circle,
+                          size: 4,
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                      Text(
+                        "${_comments[index].score} upvotes",
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 4.0),
+                  Text(
+                    _comments[index].commentPlain,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
